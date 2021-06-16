70edaa65f7f002a197382512b14f15725acc348d
presitkaur <presitkaur@users.noreply.github.com>
2021-06-17 00:15:00 +0800

    Finalised Script

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np 
 import sqlalchemy
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, MetaData, Table, Column, ForeignKey
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import datetime as dt
@@ -34,7 +34,8 @@ def home():
         f"/api/precipitation<br/>"
         f"/api/stations<br/>"
         f"/api/tobs<br/>"
-        f"/api/temp/start/end"
+        f"/api/temp/<start><br/>"
+        f"/api/temp/<start>/<end>"
 
     )
     
@@ -43,8 +44,47 @@ def home():
 def precip():
     year = dt.date(2017,8,23) - dt.timedelta(days=365)
     result = session.query(measurement.date, measurement.prcp).filter(measurement.date >= year).all()
-    precip = {date: prcp for date, prcp in result}
-    return jsonify(precip)
+    precipitation = {date: prcp for date, prcp in result}
+    return jsonify(precipitation)
+
+#Route for stations page 
+@app.route("/api/stations")
+def stations():
+    station_data = session.query(station).all()
+
+    stations_list = []
+    for station in station_data:
+        station_dict = {}
+        station_dict["id"] = station.id
+        station_dict["station"] = station.station
+        station_dict["name"] = station.name
+        station_dict["latitude"] = station.latitude
+        station_dict["longitude"] = station.longitude
+        station_dict["elevation"] = station.elevation
+        stations_list.append(station_dict)
+
+    return jsonify(stations_list)
+
+#Route for monthly temperature oberservations page 
+@app.route("/api/tobs")
+def monthly_temp():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    result = session.query(measurement.tobs).filter(measurement.station == 'USC00519281').filter(measurement.date >= prev_year).all()
+    tobs = list(np.ravel(result))
+    return jsonify(tobs=tobs)
+
+#Route for start and start/end pages 
+@app.route("/api/<start>")
+def start(start=None):
+    start = session.query(measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).group_by(measurement.date).all()
+    start_list=list(start)
+    return jsonify(start_list)
+
+@app.route("/api/temp/<start>/<end>")
+def start_end(start=None, end=None):
+    between_dates = session.query(measurement.date, func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end).group_by(measurement.date).all()
+    between_dates_list=list(between_dates)
+    return jsonify(between_dates_list)
 
 if __name__ == '__main__':
     app.run()
